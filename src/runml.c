185caf118ffff847321e7adc8c6709258ab589af
Pearlthecatgirl <97453065+Pearlthecatgirl@users.noreply.github.com>
2025-01-04 17:50:34 +0800

    Added usage message. Fixed spelling/Capitalization in error messages. Minor refactoring of error handling

--- src/runml.c
+++ src/runml.c
@@ -9,17 +9,17 @@ struct arg {
 	size_t *bcounter;
 } arg;
 
-void util_loadScriptFromPath(char *fullpath, struct arg *args);
-void util_parseMath(struct arg *args);
+int util_loadScriptFromPath(char *fullpath, struct arg *args);
+int util_parseMath(struct arg *args);
 int util_checkContains(const char *string, const char *contents);
 
-void
+int 
 util_loadScriptFromPath(char *fullpath, struct arg *args) {
 	FILE *fptr;
-	if (access(fullpath, F_OK)==-1) goto util_Load_Script_Fail; 
+	if (access(fullpath, F_OK)==-1) goto Util_Load_Script_Fail; 
 	if (!(fptr=fopen(fullpath, "r"))) {
-		fprintf(stderr, "fopen: ml file could not be opened. %d\n", 1);
-		goto util_Load_Script_Fail;
+		fprintf(stderr, "fopen: ml file could not be opened. Code: %d\n", 1);
+		goto Util_Load_Script_Fail;
 	}
 	// Determine how many lines are in the file
 	args->lines=0;
@@ -32,8 +32,8 @@ util_loadScriptFromPath(char *fullpath, struct arg *args) {
 			// reallocate args->bcounter 
 			size_t *result=realloc(args->bcounter, sizeof(size_t)*args->lines+1);
 			if (!result) {
-				fprintf(stderr, "Realloc: failed to reallocate byte counter"); 
-				goto util_Load_clean_bcounter;
+				fprintf(stderr, "Realloc: failed to reallocate byte counter. Code: %d\n", 2); 
+				goto Util_Load_clean_bcounter;
 			} args->bcounter=result;
 			args->bcounter[args->lines]=0;
 		} else args->bcounter[args->lines]+=(sizeof(size_t)*1);
@@ -44,8 +44,8 @@ util_loadScriptFromPath(char *fullpath, struct arg *args) {
 	char **result=realloc(args->code, sizeof(char *)*args->lines);
 	//printf("lines: %d\n", args->lines);
 	if (!result) {
-		fprintf(stderr, "Realloc: failed to reallocate byte counter"); 
-		goto util_Load_clean_bcounter;
+		fprintf(stderr, "Realloc: failed to reallocate byte counter. Code: %d\n", 2); 
+		goto Util_Load_clean_bcounter;
 	} args->code=result;
 
 	for (int i=0;i<args->lines;i++) {
@@ -56,28 +56,30 @@ util_loadScriptFromPath(char *fullpath, struct arg *args) {
 		// If there is an error, we want to free all previous elements before freeing the array itself
 		if (!args->code[i]) {
 			for(int j=i;j>0;j--) free(args->code[j]);
-			goto util_Load_clean_code;
+			goto Util_Load_clean_code;
 		}
 		// Copy from file
 		fgets(args->code[i],sizeof(args->bcounter[i]), fptr);
 	}
 	
-	return;	
+	return 1;	
 
-	util_Load_clean_code:
+	Util_Load_clean_code:
 	free(args->code);
-	util_Load_clean_bcounter:
+	Util_Load_clean_bcounter:
 	free(args->bcounter);
-	util_Load_Script_Fail:	
+	Util_Load_Script_Fail:	
 	free(args);
-	return;	
+	return 0;	
 } 
 
-void 
+int  
 util_parseMath(struct arg *args) {
 	for (int i=0;i<args->lines;i++) {
-		printf("lines: ");
+
+
 	}
+	return 0;
 }
 
 int 
@@ -97,7 +99,7 @@ int
 main(int argc, char **argv) {
 	char *outputName;
 	char fullpath[1024];
-	if (argc==0) goto usage;
+	if (argc<2) goto usage;
 	for (int i=1;i<argc;i++) {
 		if (argv[i][0]=='-') {
 			if (!strcmp(argv[i], "-h")) {
@@ -110,26 +112,43 @@ main(int argc, char **argv) {
 				outputName=malloc(sizeof(char)*strlen(tmpbuffer));
 				strncpy(outputName, tmpbuffer, sizeof(char)*strlen(tmpbuffer));
 			} else {
-			fprintf(stderr,"unrecognised option :\"%s\"\n", argv[i]);
+			fprintf(stderr,"unrecognised option :\"%s\". Code: %d\n", argv[i], -1);
 			goto usage;
 			}
 		} else if (util_checkContains(argv[i], ".ml")) {
 			strncpy(fullpath, argv[i], sizeof(char)*strlen(argv[i]));
-		} else fprintf(stderr,"unrecognised option :\"%s\"\n", argv[i]);
+		} else {
+			fprintf(stderr,"unrecognised option :\"%s\". Code: %d\n", argv[i], -1);
+			goto usage;	
+		}
 	}
 
 	struct arg *args=malloc(sizeof(arg));
-	util_loadScriptFromPath(fullpath, args);
-	// Check to see if util_loadScriptFromPath fails.
-	if (!args->code || !args->bcounter || !args->lines) return 1;
+	args->lines=0;
+	if (!util_loadScriptFromPath(fullpath, args)) return 1;	
 
 	// TODO: PARSE EACH LINE	
+	if (!util_parseMath(args)) {
+		if (!args->code) goto clean_args_code_and_bcounter;
+		if (!args->bcounter) goto clean_args_code_and_bcounter;
+	}
+
 
 	// TODO: TRANSLATE TO C
 	// TODO: COMPILE AND RUN
 
 
 	return 0;
+
 	usage:
-	fprintf(stderr,"Usage : runml file.ml\n");
+	fprintf(stderr,"Usage : runml [-hv] [file.ml] [-o output_name]\nProgram returns 0 on success, 1 on fail. Read the stderr code for issue.\nCode: %d\n", 0);
+	return 1;
+	clean_args_code_contents:
+	for (int i=args->lines;i>0;i--) free(args->code[i]);
+	clean_args_code_and_bcounter:
+	if (!args->code) free(args->code);
+	if (!args->bcounter) free(args->bcounter);
+	clean_args:
+	free(args);
+	return 1;
 }

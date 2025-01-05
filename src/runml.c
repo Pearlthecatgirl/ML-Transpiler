fc9176ecb9eca4a07339977ea7cb35d0c5d85bbc
Pearlthecatgirl <97453065+Pearlthecatgirl@users.noreply.github.com>
2025-01-06 00:07:03 +0800

    rewrite the whole thing because i can't figure out where the bug is

--- src/runml.c
+++ src/runml.c
@@ -1,196 +1,22 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 struct arg {
-	char **code;
-	size_t *bcounter;
-	int lines;
-
-	// Identifier value, name and sizes for both
-	char **idval;
-	char **idname;
-	int *idnamesize;
-	int *idvalsize;
-	int idvalnum, idnamenum;
+	char **id_namev;
+	size_t *id_sizev;
+	int *id_typev;
+	int idc;
+	
+	char *script;
 } arg;
 
-int util_loadScriptFromPath(char *fullpath, struct arg *args);
-int util_parseMath(struct arg *args);
-int util_checkContains(const char *string, const char *contents);
-void util_cleanArgs(struct arg *args);
+int
+util_parseMath(const char script) {
 
-int 
-util_loadScriptFromPath(char *fullpath, struct arg *args) {
-	FILE *fptr;
-	if (access(fullpath, F_OK)==-1) return 0; 
-	if (!(fptr=fopen(fullpath, "r"))) {
-		fprintf(stderr, "fopen: ml file could not be opened. Code: %d\n", 1);
-		return 0;
-	}
-	// Determine how many lines are in the file
-	args->lines=0;
-	char current;
-	args->bcounter=calloc(1, sizeof(size_t)*1); // Calloc only used to save 1 line to set the first element to 0
-	while (current!=EOF) {
-		current=fgetc(fptr);
-		if (current=='\n') {	
-			args->lines++;
-			// reallocate args->bcounter 
-			size_t *result=realloc(args->bcounter, sizeof(size_t)*args->lines+1);
-			if (!result) {
-				fprintf(stderr, "Realloc: failed to reallocate byte counter. Code: %d\n", 2); 
-				return 0;
-			} args->bcounter=result;
-			args->bcounter[args->lines]=0;
-		} else args->bcounter[args->lines]+=(sizeof(size_t)*1);
-		//printf("%c : %ld\n",current, args->bcounter[args->lines]);
-	}
 
-	fseek(fptr, 0, SEEK_SET);
-
-	char **result=realloc(args->code, sizeof(char *)*args->lines);
-	//printf("lines: %d\n", args->lines);
-	if (!result) {
-		fprintf(stderr, "Realloc: failed to reallocate byte counter. Code: %d\n", 2); 
-		return 0;
-	} args->code=result;
-
-	for (int i=0;i<args->lines;i++) {
-		//printf("sizeof line: %ld, line: %d\n", args->bcounter[i], i);
-		args->code[i]=malloc(args->bcounter[i]);
-		//printf("lines: %d\n", i);
-
-		// If there is an error, we want to free all previous elements before freeing the array itself
-		if (!args->code[i]) {
-			for(int j=i;j>0;j--) free(args->code[j]);
-			return 0;
-		}
-		// Copy from file
-		fgets(args->code[i],args->bcounter[i], fptr);
-		//printf("%s", args->code[i]);
-	}
-	
-	return 1;	
-} 
-
-int  
-util_parseMath(struct arg *args) {
-	// Loop through every line
-	char **output;
-	int resize_ret_code=0;
-	// Registering the name of the identifiers
-	char **idname=malloc(sizeof(char *)*1);
-	idname[0]="EOA";
-	short *idtype=malloc(sizeof(short)*1); //0->variable 1->function
-	int idcount=1;
-	size_t *idsize=calloc(1,sizeof(size_t));
-
-	// for (int i=0;i<args->lines;i++) {
-	// 	printf("%ld\n", args->bcounter[i]);
-	// }
-
-	for (int i=0;i<args->lines;i++) {
-		// First word in the line 
-		char *current=strtok(args->code[i], " ");
-		if (!strcmp(current, "function")) {
-			current=strtok(NULL, " ");
-			// check if the current identifier is already created
-			for (int i;i<idcount;i++) {
-				if (!strcmp(current, idname[i])) {
-					fprintf(stderr, "! runml: previous declaration of identifier (line: %d)\n", args->lines);
-					// Handle this later
-				} 
-			}
-			// Add to identifier registery
-		resize_ret_code=1; // Sets the return code so the program knows to return here on function
-		goto Util_Parse_Math_AddToCount; // Reallocates the size of each, and then returns to this line
-		Util_Parse_math_Return_Function:
-
-		idsize[idcount-1]=strlen(current);
-		idname[idcount-1]=malloc(idsize[idcount-1]);
-		if (strncpy(idname[idcount-1], current, idsize[idcount-1])) {
-		
-		}
-		//idtype[idcount-1]=1;
-
-		} else if (!strcmp(strtok(args->code[i], " "), "print")) {
-			current=strtok(NULL, " ");
-			// check if the current identifier is already created
-			char found='0';	
-			for (int i;i<idcount;i++) {
-				if (!strcmp(current, idname[i])) {
-					found='1';
-					break;
-				}
-			} 
-			if (found=='0') {
-				fprintf(stderr, "! runml: undefined identifie after print (line: %d)\n", args->lines);
-			
-			}
-		
-		} else if (!strcmp(strtok(args->code[i], " "), "#")) {
-			// Line starts with a comment
-
-		} else {
-			// Create new variable identifier or call identifier
-			current=strtok(NULL, " ");
-			// check if the current identifier is already created
-			for (int i;i<idcount;i++) {
-				if (!strcmp(current, idname[i])) {
-					fprintf(stderr, "! runml: previous declaration of identifier (line: %d)\n", args->lines);
-				}
-			}
-			resize_ret_code=0;
-			goto Util_Parse_Math_AddToCount;
-			Util_Parse_math_Return_Variable:
-			idsize[idcount-1]=strlen(current);
-			idname[idcount-1]=malloc(idsize[idcount-1]);
-			strncpy(idname[idcount-1], current, idsize[idcount-1]);
-			idtype[idcount-1]=0;
-		}
-	}
 	return 1;
-	// Clean out all mallocs made here
-	Util_Parse_Math_Clean_Identifier_Name_Register:
-	for (int i=idcount;i>0;i--) free(idname[i]);
-	Util_Parse_Math_Clean_Identifier_Name:
-	free(idname);
-	Util_Parse_Math_Clean_Identifier_Size:
-	free(idsize);
-	idcount=0;
-	return 0;
-
-	Util_Parse_Math_AddToCount:
-		idcount++;
-		char **tmp_reallocRes=realloc(idname, sizeof(char *) *(idcount+1));
-		if (!tmp_reallocRes) {
-			fprintf(stderr, "Realloc: failed to reallocate identifier registry. Code: %d\n", 2); 
-			// TODO: handle this later	
-		} idname=tmp_reallocRes;
-		size_t *tmp_reallocRes2=realloc(idsize, sizeof(size_t) * (idcount+1));
-		if (!tmp_reallocRes2) {
-			fprintf(stderr, "Realloc: failed to reallocate identifier size registry. Code: %d\n", 2); 
-			// TODO: handle this later	
-		} idsize=tmp_reallocRes2;
-		short *tmp_reallocRes3=realloc(idtype, sizeof(short) * (idcount+1));
-		if (!tmp_reallocRes3) {
-			fprintf(stderr, "Realloc: failed to reallocate identifier type registry. Code: %d\n", 2); 
-			// TODO: handle this later	
-		} idtype=tmp_reallocRes3;
-		idtype[idcount-1]=0;
-		switch (resize_ret_code) {
-			case 1:
-				goto Util_Parse_math_Return_Function;
-				break;
-			case 0:
-				goto Util_Parse_math_Return_Variable;
-				break;
-			default:
-				break;
-		}
-		return 0;
 }
 
 int 
@@ -206,58 +32,77 @@ util_checkContains(const char *string, const char *contents) {
 	return 0;
 }
 
+int 
+util_loadScript(const char *fullpath, char *script) {
+	FILE *fptr;
+	if (access(fullpath, R_OK==-1)) {
+		fprintf(stderr, "access: file (%s) either does not exist, or you do not have the permission to read from it. Code: %d\n", fullpath, 1);
+		return 0;
+	}
+	if (!(fptr=fopen(fullpath, "r"))) {
+		fprintf(stderr, "fopen: file (%s) could not be opened Code: %d\n", fullpath, 2);
+		return 0;	
+	}
+	// Get the size of the file for malloc
+	fseek(fptr, 0, SEEK_END);
+	size_t size_of_file=ftell(fptr);
+	fseek(fptr, 0, SEEK_SET);
+	
+	char *result=realloc(script, size_of_file * sizeof(char));
+	if (!result) {
+		return 0;
+	} script=result;
+
+	if (fread(script, size_of_file, 1, fptr)!=1) {
+		fprintf(stderr, "fread: size read from is different from size requested. Code: %d\n", 3);
+		goto Util_loadScript_clean_script;
+	}
+	fclose(fptr);
+	return 1;
+
+	Util_loadScript_clean_script:
+	free(script);
+	return 0;
+
+}
+
 int
 main(int argc, char **argv) {
-	char *outputName;
-	char fullpath[1024];
-	if (argc<2) goto usage;
+	char *fullpath;
+	if (argc<2) goto usage;	
 	for (int i=1;i<argc;i++) {
 		if (argv[i][0]=='-') {
 			if (!strcmp(argv[i], "-h")) {
 				goto usage;
-			} else if (!strcmp(argv[i], "-o")) {
-				// Create a temporary buffer for storing the output name before memory allocation
-				char tmpbuffer[1024];
-				strncpy(tmpbuffer, argv[++i], sizeof(char)*1024);
-				//outputLength=strlen(tmpbuffer);
-				outputName=malloc(sizeof(char)*strlen(tmpbuffer));
-				strncpy(outputName, tmpbuffer, sizeof(char)*strlen(tmpbuffer));
 			} else {
-			fprintf(stderr,"unrecognised option :\"%s\". Code: %d\n", argv[i], -1);
-			goto usage;
+				fprintf(stderr,"unrecognised option :\"%s\". Code: %d\n", argv[i], -1);
+				goto usage;
 			}
 		} else if (util_checkContains(argv[i], ".ml")) {
+			fullpath=malloc(sizeof(char)*strlen(argv[i]));
 			strncpy(fullpath, argv[i], sizeof(char)*strlen(argv[i]));
 		} else {
 			fprintf(stderr,"unrecognised option :\"%s\". Code: %d\n", argv[i], -1);
 			goto usage;	
 		}
 	}
-
 	struct arg *args=malloc(sizeof(arg));
-	args->lines=0;
-	if (!util_loadScriptFromPath(fullpath, args)) return 1;	
+	args->script=malloc(sizeof(char));
 
-	// TODO: PARSE EACH LINE	
-	if (!util_parseMath(args)) {
-		if (!args->code) goto clean_args_code_and_bcounter;
-		if (!args->bcounter) goto clean_args_code_and_bcounter;
+	if (!util_loadScript(fullpath, args->script)) {
+		goto clean_args_script;
 	}
 
-	// TODO: TRANSLATE TO C
-	// TODO: COMPILE AND RUN
+	// TODO: read from file
+	// TODO: parse file
+	// TODO: generate c file
+	// TODO: compile and run
 
-	return 0;
+return 0;
 
 	usage:
 	fprintf(stderr,"Usage : runml [-hv] [file.ml] [-o output_name]\nProgram returns 0 on success, 1 on fail. Read the stderr code for issue.\nCode: %d\n", 0);
 	return 1;
-	clean_args_code_contents:
-	for (int i=args->lines;i>0;i--) free(args->code[i]);
-	clean_args_code_and_bcounter:
-	if (!args->code) free(args->code);
-	if (!args->bcounter) free(args->bcounter);
-	clean_args:
-	free(args);
-	return 1;
+	clean_args_script:
+	free(args->script);
 }
